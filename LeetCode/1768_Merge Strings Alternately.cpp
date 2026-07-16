83067666502f7a5205e4ff38a758f8ac2bf59f85
yashdarunkar1515 <yashbd900@gmail.com>
2026-07-16 18:44:10 +0530

    Add mergeAlternately function for string merging
    Implement mergeAlternately function to merge two strings alternately, handling different lengths.

--- LeetCode/1768_Merge Strings Alternately.cpp
+++ LeetCode/1768_Merge Strings Alternately.cpp
@@ -1,6 +1,41 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string sb;
 
-Code
-Testcase
-Testcase
-Test Result
-1768. Merge Strings Alternately
+        if(word1.size()>= word2.size())
+        {
+            for(int i=0;i<word1.size();i++)
+            {
+                if(i<word2.size())
+                {
+                    sb=sb+word1[i];
+                    sb=sb+word2[i];
+                }
+                if(i>=word2.size())
+                {
+                    sb=sb+word1[i];
+                }
+            }
+        }
+
+        if(word2.size()>word1.size())
+        {
+            for(int i=0;i<word2.size();i++)
+            {
+                if(i<word1.size())
+                {
+                    sb=sb+word1[i];
+                    sb=sb+word2[i];
+
+                }
+                if(i>=word1.size())
+                {
+                     sb=sb+word2[i];
+
+                }
+            }
+        }
+    return sb;
+    }
+};
